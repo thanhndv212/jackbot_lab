61af39d53e33182601ba12d97deff697df5eb19b
thanhndv212 <thanhndv212@gmail.com>
2025-03-14 13:51:40 +0100

    change flat orientation weight

--- source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
+++ source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
@@ -31,7 +31,7 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
         self.rewards.lin_vel_z_l2.weight = -0.005
 
         self.rewards.ang_vel_xy_l2.weight = -0.05
-        self.rewards.flat_orientation_l2.weight = -0.5
+        self.rewards.flat_orientation_l2.weight = -5.0
 
         self.rewards.base_height_l2.weight = -0.5
         self.rewards.base_height_l2.params["target_height"] = 0.81
