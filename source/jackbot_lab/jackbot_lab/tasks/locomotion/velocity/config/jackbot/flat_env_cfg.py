2c1a9d683919e5b68b12413c8b9c58f4d6d1e9c3
thanhndv212 <thanhndv212@gmail.com>
2025-03-17 23:32:58 +0100

    reduce base height weight by 100

--- source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
+++ source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
@@ -33,7 +33,7 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
         self.rewards.ang_vel_xy_l2.weight = -0.05
         self.rewards.flat_orientation_l2.weight = -5.0
 
-        self.rewards.base_height_l2.weight = -0.5
+        self.rewards.base_height_l2.weight = -0.005
         self.rewards.base_height_l2.params["target_height"] = 0.832
         self.rewards.base_height_l2.params["asset_cfg"].body_names = [
             self.base_link_name
