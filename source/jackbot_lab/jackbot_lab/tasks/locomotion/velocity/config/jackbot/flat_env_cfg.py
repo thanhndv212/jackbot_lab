89140a935939942396a5e0b1208bc4167a69e9d1
thanhndv212 <thanhndv212@gmail.com>
2025-03-17 23:32:26 +0100

    change body names in joint acc l2

--- source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
+++ source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
@@ -73,9 +73,9 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
         self.rewards.joint_acc_l2.params["asset_cfg"] = SceneEntityCfg(
             "robot",
             joint_names=[
-                # ".*_hip_.*",
-                # ".*_knee_.*",
-                ".*_ankle_.*",
+                ".*_hip_.*",
+                ".*_knee_.*",
+                # ".*_ankle_.*",
             ],
         )
         self.rewards.joint_torques_l2.weight = -1.5e-7
