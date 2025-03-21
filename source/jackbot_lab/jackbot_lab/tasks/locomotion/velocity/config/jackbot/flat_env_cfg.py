e6288d83e3abe56c2865d6a49734fad563af76cc
thanhndv212 <thanhndv212@gmail.com>
2025-03-21 09:57:19 +0100

    reset all weights

--- source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
+++ source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
@@ -27,37 +27,42 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
         # General
         self.rewards.is_terminated.weight = -200
 
-        # Root penalties
-        self.rewards.lin_vel_z_l2.weight = -0.05
+        # Velocity-tracking rewards
+        self.rewards.track_lin_vel_xy_exp.weight = 2.0
+        self.rewards.track_lin_vel_xy_exp.params["std"] = 0.5
 
-        self.rewards.ang_vel_xy_l2.weight = -0.05
+        self.rewards.track_ang_vel_z_exp.weight = 2.0
+        self.rewards.track_ang_vel_z_exp.params["std"] = 0.5
+
+        # Root penalties
+        self.rewards.lin_vel_z_l2.weight = -0.0
+
+        self.rewards.ang_vel_xy_l2.weight = -0.0
+
         self.rewards.flat_orientation_l2.weight = -5.0
 
         self.rewards.base_height_l2.weight = -0.5
-        self.rewards.base_height_l2.params["target_height"] = 0.81
-        self.rewards.base_height_l2.params["asset_cfg"].body_names = [
-            self.base_link_name
-        ]
+        self.rewards.base_height_l2.params["target_height"] = 0.84
 
         self.rewards.body_lin_acc_l2.weight = 0
 
         # Joint penalties
-        self.rewards.joint_deviation.weight = -0.0
+        self.rewards.joint_deviation.weight = -0.1
         self.rewards.create_joint_deviation_l1_rewterm(
             "joint_deviation_other_l1",
-            -0.5,
+            -0.0,
             [
                 ".*_yaw_hip_joint",
                 ".*_yaw_knee_joint",
                 ".*_shoulder_joint",
                 ".*_pitch_elbow_joint",
                 ".*_wrist_joint",
-                ".*_roll_hip_joint", 
+                ".*_roll_hip_joint",
                 "yaw_waist_joint",
             ],
         )
         self.rewards.create_joint_deviation_l1_rewterm(
-            "joint_deviation_knee_l1", -0.1, [".*_pitch_knee_joint"]
+            "joint_deviation_knee_l1", -0.0, [".*_pitch_knee_joint"]
         )
         self.rewards.create_joint_deviation_l1_rewterm(
             "joint_deviation_hip_l1",
@@ -66,10 +71,12 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
         )
         self.rewards.create_joint_deviation_l1_rewterm(
             "joint_deviation_ankle_l1",
-            -0.1,
+            -0.0,
             [".*_pitch_ankle_joint", ".*_roll_ankle_joint"],
         )
-        self.rewards.joint_acc_l2.weight = -1.0e-7
+        # self.rewards.joint_acc_l2.weight = -1.0e-7
+        self.rewards.joint_acc_l2.weight = -0.0
+
         self.rewards.joint_acc_l2.params["asset_cfg"] = SceneEntityCfg(
             "robot",
             joint_names=[
@@ -78,7 +85,9 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
                 # ".*_ankle_.*",
             ],
         )
-        self.rewards.joint_torques_l2.weight = -1.5e-7
+        # self.rewards.joint_torques_l2.weight = -1.5e-7
+        self.rewards.joint_torques_l2.weight = -0.0
+
         self.rewards.joint_torques_l2.params["asset_cfg"] = SceneEntityCfg(
             "robot",
             joint_names=[
@@ -89,33 +98,44 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
         )
 
         # Action penalties
-        self.rewards.action_rate_l2.weight = -0.005
+        self.rewards.action_rate_l2.weight = -0.00
 
         # Contact sensor
         self.rewards.undesired_contacts.weight = 0
         self.rewards.contact_forces.weight = 0
 
-        # Velocity-tracking rewards
-        self.rewards.track_lin_vel_xy_exp.weight = 2.0
-        self.rewards.track_lin_vel_xy_exp.params["std"] = 0.5
-
-        self.rewards.track_ang_vel_z_exp.weight = 2.0
-        self.rewards.track_ang_vel_z_exp.params["std"] = 0.5
-
         # Feet rewards
-        self.rewards.feet_air_time.weight = 1.0
+        self.rewards.feet_air_time.weight = 0.0
 
         self.rewards.feet_air_time.params["threshold"] = 0.5
 
         self.rewards.feet_contact.weight = 0.0
 
-        self.rewards.feet_slide.weight = -0.05
+        self.rewards.feet_slide.weight = -0.0
 
-        # Others
-        self.rewards.joint_power.weight = 0
-        self.rewards.stand_still_without_cmd.weight = 0.0
+        self.rewards.knee_keep_distance.weight = -0.0
 
-        # Commands
+        self.rewards.feet_keep_distance.weight = -0.0
+
+        self.rewards.right_foot_orientaion.weight = -0.0
+
+        self.rewards.left_foot_orientaion.weight = -0.0
+
+        self.rewards.clock_frc.weight = 0.0
+
+        self.rewards.clock_vel.weight = 0.0
+
+        self.rewards.dof_pos_limits.weight = -0.0
+        self.rewards.dof_pos_limits.params["asset_cfg"] = SceneEntityCfg(
+            "robot",
+            joint_names=[
+                ".*_hip_.*",
+                ".*_knee_.*",
+                ".*_ankle_.*",
+            ],
+        )
+
+        # ------------------------------Commands------------------------------
         self.commands.base_velocity.ranges.lin_vel_x = (0.0, 1.0)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.0, 0.0)
         self.commands.base_velocity.ranges.ang_vel_z = (-0.5, 0.5)
