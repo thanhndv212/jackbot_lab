90f9f448771266e415828b22883556dbf701bf47
thanhndv212 <thanhndv212@gmail.com>
2025-04-04 01:36:31 +0200

    root penalty weights

--- source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
+++ source/jackbot_lab/jackbot_lab/tasks/locomotion/velocity/config/jackbot/flat_env_cfg.py
@@ -35,13 +35,14 @@ class JackbotFlatEnvCfg(JackbotRoughEnvCfg):
         self.rewards.track_ang_vel_z_exp.weight = 0.6
         self.rewards.track_ang_vel_z_exp.params["std"] = 0.5
 
-        # Root penalties
-        self.rewards.lin_vel_z_l2.weight = -0.0
-        self.rewards.ang_vel_xy_l2.weight = -0.0
-        self.rewards.flat_orientation_l2.weight = -5.0
-        self.rewards.base_height_exp.weight = -2.0
+        # Root penalties (essential for stability)
+        self.rewards.lin_vel_z_l2.weight = -0.8
+        self.rewards.ang_vel_xy_l2.weight = -0.6
+        self.rewards.flat_orientation_l2.weight = -0.6
+        self.rewards.base_height_exp.weight = -1.5
         self.rewards.base_height_exp.params["target_height"] = 0.828
-        self.rewards.body_lin_acc_l2.weight = 0
+        self.rewards.base_height_exp.params["std"] = 0.2
+        self.rewards.body_lin_acc_l2.weight = -0.3
 
         # Joint penalties
         self.rewards.joint_deviation.weight = -0.0
